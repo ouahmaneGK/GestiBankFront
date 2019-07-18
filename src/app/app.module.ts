0a8d031b88b694e87fccfcdfdda63ef0a5fb115c
ouahmaneGK <o.ouahmane@gmail.com>
2019-07-18 13:55:37 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientComponent } from './User/client/client.component';
+import { ConseillerComponent } from './User/conseiller/conseiller.component';
+import { AdministrateurComponent } from './User/administrateur/administrateur.component';
+import { CompteComponent } from './compte/compte.component';
+import { ClientPotentielComponent } from './client-potentiel/client-potentiel.component';
+import { RequeteComponent } from './requete/requete.component';
+import { DemandeOuvertureComponent } from './demande-ouverture/demande-ouverture.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    ConseillerComponent,
+    AdministrateurComponent,
+    CompteComponent,
+    ClientPotentielComponent,
+    RequeteComponent,
+    DemandeOuvertureComponent,
+    TransactionComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/requete/requete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-requete',
+  templateUrl: './requete.component.html',
+  styleUrls: ['./requete.component.css']
+})
+export class RequeteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
